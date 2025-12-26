6f25c5569422a9e9a4aa9aed8e75a92a29626cf9
capybaraee <reggidyazess@gmail.com>
2025-12-26 22:58:59 +0700

    Refactor Borrowable interface methods to include Member

--- src/java/com/ereadly/interfaces/Borrowable.java
+++ src/java/com/ereadly/interfaces/Borrowable.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.ereadly.interfaces;
 
-/**
- *
- * @author Luluil Maknun
- */
+import com.ereadly.model.Member;
+
 public interface Borrowable {
-    void borrowBook();
-    void returnBook();
+    void borrow(Member member);
+    void returnBook(Member member);
 }
